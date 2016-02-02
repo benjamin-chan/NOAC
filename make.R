7df879bffb4ad12641c276f5c9455a4c0910d742
Benjamin Chan <chanb@ohsu.edu>
2016-02-01 16:40:37 -0800

    Reorganize epilogue for reproducibility

--- make.R
+++ make.R
@@ -4,8 +4,13 @@ sink(f)
 rmarkdown::render("MASTER.Rmd", output_file="index.html")
 file.rename("index.md", "README.md")
 file.info(c("index.html", "README.md"))
+.timeStart <- Sys.time()
 rmarkdown::render("mtcMortality.Rmd")
 rmarkdown::render("mtcStroke.Rmd")
 rmarkdown::render("mtcMI.Rmd")
 rmarkdown::render("mtcBleeding.Rmd")
+library(devtools)
+source_gist("https://gist.github.com/benjamin-chan/80149dd4cdb16b2760ec",
+            print.eval=TRUE,
+            quiet=TRUE)
 sink(NULL)
